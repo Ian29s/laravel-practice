719c88605736140a15d0b78b4af0841af33a7829
Codeian <106393012+Ian29s@users.noreply.github.com>
2025-08-20 15:58:19 +0800

    Add UserController and new web routes
    Introduced a basic UserController with an index method returning a test string. Added new routes for /greeting, /user (using UserController), and a redirect from /here to /theress in web.php.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+
+Route::get('/user', [UserController::class, 'index']);
+
+Route::redirect('/here', '/theress');
